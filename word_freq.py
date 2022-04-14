ff85ccc121c2b0c3fe5ac6af778764d46f034213
sunjuHong <dasoni18118@gmail.com>
2022-04-14 21:04:14 +0900

    word_freq.py added

---
+++ word_freq.py
@@ -0,0 +1,31 @@
+#!user/bin/python
+import sys
+
+#HW05_Python Programming
+#practice) word frequency
+
+f=open(sys.argv[1],"r")
+list1=f.readlines()
+list2=[]
+for i in range(len(list1)):
+	list1[i]=list1[i].split()
+	list2=list2+list1[i]
+
+word_count={}
+for word in list2:
+	if word in word_count:
+		word_count[word]+=1
+	else:
+		word_count[word]=1
+word_count=sorted(word_count.items(),key=lambda item:item[1],reverse=True)
+
+for i in range(int(sys.argv[2])):
+	k=word_count[i]
+	k=list(k)
+	k[1]=str(k[1])
+	k[0]
+	print("{:<5}".format(k[0]),end=" ")
+	print("{:>5}".format(k[1]))
+
+f.close()
+
